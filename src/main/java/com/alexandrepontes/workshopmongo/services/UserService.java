0b8afdb97ed9ebc8063c6df54330586c4e2b6c09
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2020-01-11 19:06:04 -0300

    Obtendo um usuário por id

--- src/main/java/com/alexandrepontes/workshopmongo/services/UserService.java
+++ src/main/java/com/alexandrepontes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.alexandrepontes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alexandrepontes.workshopmongo.domain.User;
 import com.alexandrepontes.workshopmongo.repository.UserRepository;
+import com.alexandrepontes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service 
 	public class UserService {
@@ -17,4 +19,9 @@ import com.alexandrepontes.workshopmongo.repository.UserRepository;
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
---
+++ src/main/java/com/alexandrepontes/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.alexandrepontes.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+		public ObjectNotFoundException(String msg) {
+			super(msg);
+		}
+	
+}
