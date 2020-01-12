e5d1025365baff59d55c71a0f8549fb0030e4f78
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2020-01-12 09:04:06 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/alexandrepontes/workshopmongo/services/UserService.java
+++ src/main/java/com/alexandrepontes/workshopmongo/services/UserService.java
@@ -11,29 +11,34 @@ import com.alexandrepontes.workshopmongo.dto.UserDTO;
 import com.alexandrepontes.workshopmongo.repository.UserRepository;
 import com.alexandrepontes.workshopmongo.services.exception.ObjectNotFoundException;
 
-@Service 
-	public class UserService {
-	
+@Service
+public class UserService {
+
 	@Autowired
-	private UserRepository repo; //automatic dependencies injection
-	
-	public List<User> findAll(){
+	private UserRepository repo; // automatic dependencies injection
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
-	
+	}
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+
+		findById(id);
+		repo.deleteById(id);
+
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
-	
-	
+
 }
