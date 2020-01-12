c0c6519521f2a2e9bfc39f76c990a582b454e29c
Alexandre Pontes <alexandrepontesgyn@gmail.com>
2020-01-12 11:52:51 -0300

    Obtendo um post por id

---
+++ src/main/java/com/alexandrepontes/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.alexandrepontes.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandrepontes.workshopmongo.domain.Post;
+import com.alexandrepontes.workshopmongo.repository.PostRepository;
+import com.alexandrepontes.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo; // automatic dependencies injection
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
